c01a2935b93c682909e80e74ab97c28f09596d41
Jeanfranco Paolo <jeanfranko.pci16@gmail.com>
2018-05-10 17:39:13 -0500

    Modificaciones en los módulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,16 @@
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const routes: Routes = [
-    { path: '', component: PagesComponent,
-      children: [
-        { path: 'dashboard', component: DashboardComponent },
-        { path: 'progress', component: ProgressComponent },
-        { path: 'graficas1', component: Graficas1Component },
-        { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-      ]
-    },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent}
 ];
-
-// @NgModule({
-//     imports: [RouterModule.forChild(routes)],
-//     exports: [RouterModule]
-// })
-// export class FeatureRoutingModule {}
 
 export const APP_ROUTES = RouterModule.forRoot(routes, { useHash: true });
